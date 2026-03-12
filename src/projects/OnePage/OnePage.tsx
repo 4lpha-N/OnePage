562cdb7c3c83b72e6653abe0b072204e44a8c0c6
Nikolai Rutow <nru@kittelberger.de>
2026-03-12 13:42:05 +0100

    [main] cleanup + favicon

--- src/projects/OnePage/OnePage.tsx
+++ src/projects/OnePage/OnePage.tsx
@@ -53,10 +53,10 @@ function HomeContent() {
           </Grid>
       </Grid>
       <Grid container spacing={4}>
-            <Grid size={{ xs: 12, sm: 6, md: 6, lg: 6, xl: 6 }} style={{backgroundColor: 'crimson'}} display={'block'}>
+            <Grid size={{ xs: 12, sm: 6, md: 6, lg: 6, xl: 6 }} display={'block'}>
               <span>Col 1</span>
             </Grid>
-            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }} offset={{ xs: 0, md: 2, lg: 3, xl: 4 }} style={{backgroundColor: 'crimson'}}>
+            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }} offset={{ xs: 0, md: 2, lg: 3, xl: 4 }}>
               <span>Col 2</span>
             </Grid>
         </Grid>
