34f6851287d8622c3ad7ead4161c962fb2dd20d4
Nikolai Rutow <nru@kittelberger.de>
2026-03-19 15:00:45 +0100

    [main] modal + zoom

--- src/projects/OnePage/components/Modal/Modal.tsx
+++ src/projects/OnePage/components/Modal/Modal.tsx
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Zoom } from '@mui/material'; // Collapse, Fade, Grow, Slide,
 import { IoCloseOutline } from 'react-icons/io5';
 
 interface ModalComponentProps {
@@ -32,24 +33,26 @@ export default function ModalComponent({
         aria-describedby="modal-modal-description"
         className="onepage-modal"
       >
-        <Box className="onepage-modal__box">
-          <Box className="onepage-modal__head">
-            <Typography id="onepage-modal__title" variant="h6" component="h2">
-              {title || ''}
-            </Typography>
-            <Box className="onepage-modal__close">
-              <IconButton
-                aria-label="Close modal"
-                color="secondary"
-                size="small"
-                onClick={handleClose}
-              >
-                <IoCloseOutline size={32} />
-              </IconButton>
+        <Zoom in={open} timeout={500}>
+          <Box className="onepage-modal__box">
+            <Box className="onepage-modal__head">
+              <Typography id="onepage-modal__title" variant="h6" component="h2">
+                {title || ''}
+              </Typography>
+              <Box className="onepage-modal__close">
+                <IconButton
+                  aria-label="Close modal"
+                  color="secondary"
+                  size="small"
+                  onClick={handleClose}
+                >
+                  <IoCloseOutline size={32} />
+                </IconButton>
+              </Box>
             </Box>
+            <Box>{children}</Box>
           </Box>
-          <Box>{children}</Box>
-        </Box>
+        </Zoom>
       </Modal>
     </div>
   );
